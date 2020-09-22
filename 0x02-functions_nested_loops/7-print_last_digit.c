19e9d944b5682ed6457173f2d081fb8c339160d9
YourFavouriteOreo <adiwani98@gmail.com>
2020-09-22 07:33:44 +0400

    attempted fix task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
+
 /**
  * print_last_digit - prints and return the last digit of an int
  * @n: integer from which to extract the last digit
- * Return: int.
+ *
+ * Return: Last digit value.
  */
 
 int print_last_digit(int n)
 {
-	int last_digit;
+	int retval;
 
 	if (n < 0)
 	{
 		n *= -1;
 	}
-	last_digit = n % 10;
-	_putchar(last_digit + '0');
-	return (last_digit);
+	retval = n % 10;
+	_putchar(retval + '0');
+	return (retval);
 }

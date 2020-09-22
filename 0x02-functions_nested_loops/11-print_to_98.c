7023ad2af027543f05d33e5a45f8aabd516f7bd3
YourFavouriteOreo <adiwani98@gmail.com>
2020-09-22 07:49:38 +0400

    fix task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,23 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if (i != n)
+			{
+				printf(", ");
+			}
+			printf("%d", i);
 		}
 	}
 	else
 	{
 		for (i = n; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i != n)
+			{
+				printf(", ");
+			}
+			printf("%d", i);
 		}
 	}
+	putchar('\n');
 }

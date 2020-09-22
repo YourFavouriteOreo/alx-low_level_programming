0d146cc4aec7e3017c3e5e0f0d5f258a45ffb91d
YourFavouriteOreo <adiwani98@gmail.com>
2020-09-22 07:37:31 +0400

    attempted fix task 8

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 extern int _putchar(char c);
+extern void jack_bauer(void);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 extern int _islower(int c);
